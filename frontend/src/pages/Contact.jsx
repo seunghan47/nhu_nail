1056a537251308f541761c273635d2bbe46d0779
Paul Lim <paulslim47@gmail.com>
2024-01-23 15:43:37 -0500

    contact info update

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -3,54 +3,69 @@ import styles from "./Contact.module.css";
 const Contact = () => {
   return (
     <div className={styles.contact}>
-      <div className={styles.flex_container}>
-        <img
-          src=''
-          alt='calming room'
-        />
-        <form>
-          <h2>Have Questions ? </h2>
-          <h4>Don't hesitate to Reach out!</h4>
+      <form>
+        <h3>
+          <span className={styles.title}>Have Questions ? </span>
+        </h3>
+        <h3>{/* <span className={styles.sub_title}>Don't hesitate to Reach out! </span> */}</h3>
 
-          <div className='control-row'>
-            <div className='control'>
-              <label htmlFor='first-name'>First Name</label>
-              <input
-                type='text'
-                id='first-name'
-                name='first-name'
-              />
-            </div>
+        <div className='control'>
+          <label htmlFor='name'>Name</label>
+          <input
+            placeholder='Jane Doe'
+            type='text'
+            id='name'
+            name='name'
+          />
+        </div>
 
-            <div className='control'>
-              <label htmlFor='last-name'>Last Name</label>
-              <input
-                type='text'
-                id='last-name'
-                name='last-name'
-              />
-            </div>
-          </div>
+        <div className='control'>
+          <label htmlFor='email'>Email</label>
+          <input
+            placeholder='Janedoe@gmail.com'
+            id='email'
+            type='email'
+            name='email'
+          />
+        </div>
 
-          <div className='control'>
-            <label htmlFor='email'>Email</label>
-            <input
-              id='email'
-              type='email'
-              name='email'
-            />
-          </div>
+        <div className={styles.control}>
+          <label htmlFor='number'>Phone Number</label>
+          <input
+            placeholder='111-111-1111'
+            id='number'
+            type='text'
+            name='number'
+          />
+        </div>
 
-          <div className={styles.control}>
-            <label htmlFor='number'></label>
-            <input
-              id='number'
-              type='text'
-              name='number'
-            />
-          </div>
-        </form>
-      </div>
+        <div className={styles.control}>
+          <label htmlFor='subject'>Subject</label>
+          <input
+            placeholder='Pricing / Specific style'
+            id='subject'
+            type='text'
+            name='subject'
+          />
+        </div>
+
+        <div className={styles.control}>
+          <label htmlFor='message'>Message</label>
+          <textarea
+            className={styles.message}
+            placeholder='Is [name of style] something you can do? '
+            id='message'
+            type='text'
+            name='message'
+          />
+        </div>
+        <button
+          className={styles.button}
+          type='button'
+        >
+          Send!
+        </button>
+      </form>
     </div>
   );
 };
